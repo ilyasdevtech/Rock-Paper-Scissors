bb06992ebcc7eb9891bbc43510d45d78f219fb60
Ilyasdevtech <ilyas.mukhambetzhanov@gmail.com>
2026-02-17 01:06:17 +0500

    Refactor getComputerChoice function to use switch statement for random choice generation

--- main.js
+++ main.js
@@ -2,10 +2,29 @@
 // It creates an array of possible choices, selects a random index, 
 // and returns the corresponding choice.
 
+// function getComputerChoice() {
+//     const choices = ['rock', 'paper', 'scissors'];
+//     const randomIndex = Math.floor(Math.random() * choices.length);
+//     return choices[randomIndex];
+// }
+
+// console.log(getComputerChoice());
+
+// Method 2. This function achieves the same result using a switch statement instead of an array.
+
 function getComputerChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
+    const randomIndex = Math.floor(Math.random() * 3);
+    switch (randomIndex) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+        default:
+            break;
+    }
 }
 
 console.log(getComputerChoice());
+
